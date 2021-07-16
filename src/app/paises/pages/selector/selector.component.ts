7c9b91e713e641bf341bd5f17251a2fd3c0c0287
Franco Oliva <franco.oliva1991@gmail.com>
2021-07-16 11:19:23 -0300

    Continuar con la clase para llenar el tercer selector

--- src/app/paises/pages/selector/selector.component.ts
+++ src/app/paises/pages/selector/selector.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// pipe - nos sirve para implementar métodos rxjs
+// switchMap - toma el valor producto de un observable y a su vez lo muta 
+// y regresa el valor de otro observable
+import { switchMap, tap } from 'rxjs/operators';
+
 import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interface/paises.interface';
+
+
 
 @Component({
   selector: 'app-selector',
@@ -11,17 +20,42 @@ import { PaisesService } from '../../services/paises.service';
 export class SelectorComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    region: ['', Validators.required ]
+    region: ['', Validators.required ],
+    pais: ['', Validators.required ],
+    frontera: ['', Validators.required ]
   });
 
   // llenar selector regiones
   regiones: string[] = [];
+  paises: PaisSmall[] = [];
 
   constructor( private fb: FormBuilder, private paisesService: PaisesService ) { }
 
   ngOnInit(): void {
     
     this.regiones = this.paisesService.regiones;
+
+    // Cuando cambia la región
+    this.miFormulario.get('region')?.valueChanges
+    .pipe( 
+      tap( _ => {
+
+        this.miFormulario.get('pais')?.reset('');
+
+      }),
+      switchMap( region => this.paisesService.getPaisesPorRegion( region ))
+    )
+    .subscribe( paises => {
+
+      this.paises = paises;
+      
+    });
+
+    // Cuando cambia el país
+    this.miFormulario.get('pais')?.valueChanges
+    .subscribe( codigo => {
+      console.log(codigo);
+    });
 
   }
 
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,15 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+// of nos permite devolver un nuevo observable
+import { Observable, of } from 'rxjs';
+
+import { Pais, PaisSmall } from '../interface/paises.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
+  private baseUrl: string = 'https://restcountries.eu/rest/v2';
   private _regiones: string[] = [ 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania' ];
 
   get regiones(): string[] {
     return [ ...this._regiones ];
   }
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getPaisesPorRegion( region:string ): Observable<PaisSmall[]>{
+    
+    const url: string = `${ this.baseUrl }/region/${region}?fields=alpha3Code;name`;
+
+    return this.http.get<PaisSmall[]>(`${ url }`);
+  }
+
+  getPaisPorCodigo( codigo:string ): Observable<Pais | null>{
+
+    // En el caso de recibir un argumento vacío
+    if ( !codigo ){
+
+      return of(null);
+      
+    }
+
+    const url = `${ this.baseUrl }/alpha/${ codigo }`;
+
+    return this.http.get<Pais>( url );
+  }
 }
